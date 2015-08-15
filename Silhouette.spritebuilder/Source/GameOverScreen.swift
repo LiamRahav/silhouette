b3187e9109f720c4ee1933be973c7ff867f2daf7
liamrahav <liorogamerdvd@gmail.com>
2015-08-15 09:38:55 -0400

    important updates

---
+++ Silhouette.spritebuilder/Source/GameOverScreen.swift
@@ -0,0 +1,32 @@
+//
+// Silhouette (c) by Liam Rahav
+//
+// Silhouette is licensed under a
+// Creative Commons Attribution-NonCommercial 4.0 International License.
+
+
+import Foundation
+
+class GameOverScreen: CCNode {
+  weak var highscoreLabel: CCLabelTTF!
+  weak var scoreLabel: CCLabelTTF!
+  var parentGameScene: GameScene!
+  
+  func playAgain() {
+    let newScene = CCBReader.loadAsScene("GameScene")
+    CCDirector.sharedDirector().replaceScene(newScene)
+  }
+  
+  func returnToMenu() {
+    let newScene = CCBReader.loadAsScene("MenuScene")
+    CCDirector.sharedDirector().replaceScene(newScene)
+  }
+  
+  func share() {
+    Mixpanel.sharedInstance().track("Share Button Pressed")
+    
+    let formattedScore = NSString(format: "%.1f", self.parentGameScene.score)
+    // String that goes in the share function
+    SharingManager.sharedInstance.share("I just got \(formattedScore)m on Silhouette! Think you can beat it? Click this link to find out! " + SharingManager.sharedInstance.defaultURL)
+  }
+}
